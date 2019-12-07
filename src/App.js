00d90c3a3b3ea8584c2426d250e05375e68bb9e6
Алексей Горобцов <aleksejgorobcov@MacBook-Pro-Aleksej.local>
2019-12-07 21:32:07 +0300

    rules of hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-import { Example } from "./component/counter";
+import { Example, Form } from "./component/counter";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Example />
+      <Form/>
     </div>
   );
 }
--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -3,7 +3,7 @@ import React, {useState, useEffect} from 'react';
 export function Example() {
     const [count, setCount] = useState(0);
     useEffect(() => {
-       document.title = `You clicked ${count} times`;
+        document.title = `You clicked ${count} times`;
     });
 
 
@@ -20,4 +20,33 @@ function ExampleWithManyStates() {
     const [age, setAge] = useState(42);
     const [fruit, setFruit] = useState('banana');
     const [todos, setTodos] = useState([{text: 'Learn Hooks'}])
+}
+
+export function Form() {
+    const [name, setName] = useState('Mary');
+
+    useEffect(function persistForm() {
+        if (name) {
+            localStorage.setItem('formData', name);
+        }
+
+    });
+
+    const [surname, setSurname] = useState('Poppins');
+
+    useEffect(function updateTitle() {
+        document.title = `${name} ${surname}`;
+    });
+
+    function handleName(e) {
+        console.log(e.target.value);
+        setName(e.target.value);
+    }
+
+    return (
+        <div>
+            <input value={name} onChange={handleName}/>
+            <p>{surname}</p>
+        </div>
+    )
 }
